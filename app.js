f1cfada612fc15abc3ee772d12f2913d865ac99c
Anzumana Taal <anzumana.taal@me.com>
2017-06-09 10:55:11 +0200

    update overwatch

--- app.js
+++ app.js
@@ -16,26 +16,26 @@ myApp.controller('SquadCtrl', ['$scope', function TodoCtrl($scope) {
 	$scope.data.squad["twix"].name="Twixman#2220";
 
 	$scope.data.squad["anzumana"].championpool= [
-	{name:'Junkrat', playing:true,training:false, icon:'icon junkrat',highlight:'http://youtube.com',title:'Tire action'},
-	{name:'Hanzo', playing:true,training:false, icon:'icon hanzo',highlight:'http://youtube.com',title:'simple geometry'},
-	{name:'Phara', playing:true,training:false, icon:'icon phara',highlight:'http://youtube.com',title:'justice rains'},
-	{name:'Lucio', playing:true,training:false, icon:'icon lucio',highlight:'http://youtube.com',title:'that save'},
-	{name:'DVA', playing:true,training:false, icon:'icon dva',highlight:'http://youtube.com',title:'that save'},
-	{name:'Genji', playing:false,training:false, icon:'icon genji',highlight:'http://youtube.com',title:'that save'},
-	{name:'Tracer', playing:false,training:true, icon:'icon tracer',highlight:'http://youtube.com',title:'that save'},
-	{name:'Winston', playing:false,training:true, icon:'icon winston',highlight:'http://youtube.com',title:'that save'},
-	{name:'Soldier', playing:false,training:true, icon:'icon soldier',highlight:'http://youtube.com',title:'that save'},
+	{name:'anna', playing:true,training:false, highlight:'http://youtube.com',title:'Tire action'},
+	{name:'hanzo', playing:true,training:false, highlight:'http://youtube.com',title:'simple geometry'},
+	{name:'phara', playing:true,training:false, highlight:'http://youtube.com',title:'justice rains'},
+	{name:'lucio', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+	{name:'dva', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+	{name:'genji', playing:false,training:false, highlight:'http://youtube.com',title:'that save'},
+	{name:'tracer', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
+	{name:'winston', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
+	{name:'soldier', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
 	];
 	$scope.data.squad["jayjay"].championpool= [
-		{name:'McCree', playing:true,training:false, icon:'icon mccree',highlight:'http://youtube.com',title:'that save'},
-		{name:'Zeniata', playing:true,training:false, icon:'icon zeniata',highlight:'http://youtube.com',title:'that save'},
-		{name:'Soldier', playing:true,training:false, icon:'icon soldier',highlight:'http://youtube.com',title:'that save'},
-		{name:'Reinhard', playing:true,training:false, icon:'icon reinhard',highlight:'http://youtube.com',title:'that save'}
+		{name:'mccree', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 	$scope.data.squad["twix"].championpool= [
-		{name:'McCree', playing:true,training:false, icon:'icon mccree',highlight:'http://youtube.com',title:'that save'},
-		{name:'Zeniata', playing:true,training:false, icon:'icon zeniata',highlight:'http://youtube.com',title:'that save'},
-		{name:'Soldier', playing:true,training:false, icon:'icon soldier',highlight:'http://youtube.com',title:'that save'},
-		{name:'Reinhard', playing:true,training:false, icon:'icon reinhard',highlight:'http://youtube.com',title:'that save'}
+		{name:'mccree', playing:true,training:false, title:'that save'},
+		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 }]);
