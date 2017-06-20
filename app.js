249d2802916c770420ce574759e63c49abe94709
Anzumana Taal <anzumana.taal@me.com>
2017-06-20 18:47:32 +0200

    overwatch squad info added

--- app.js
+++ app.js
@@ -10,32 +10,41 @@ myApp.controller('SquadCtrl', ['$scope', function TodoCtrl($scope) {
 	$scope.data.squad={};
 	$scope.data.squad["anzumana"]={};
 	$scope.data.squad["jayjay"]={};
-	$scope.data.squad["twix"]={};
+	$scope.data.squad["sauvrinn"]={};
+	$scope.data.squad["lewcor"]={};
+	$scope.data.squad["clowntown"]={};
 	$scope.data.squad["anzumana"].name="Senwyn#2415";
 	$scope.data.squad["jayjay"].name="JayJayBiggie#2793";
-	$scope.data.squad["twix"].name="Twixman#2220";
+	$scope.data.squad["sauvrinn"].name="Sauvrin#1244";
+	$scope.data.squad["lewcor"].name="lewcor12#";
+	$scope.data.squad["clowntown"].name="Myles#1857";
 
 	$scope.data.squad["anzumana"].championpool= [
-	{name:'anna', playing:true,training:false, highlight:'http://youtube.com',title:'Tire action'},
-	{name:'hanzo', playing:true,training:false, highlight:'http://youtube.com',title:'simple geometry'},
-	{name:'phara', playing:true,training:false, highlight:'http://youtube.com',title:'justice rains'},
-	{name:'lucio', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-	{name:'dva', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-	{name:'genji', playing:false,training:false, highlight:'http://youtube.com',title:'that save'},
-	{name:'tracer', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
-	{name:'winston', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
-	{name:'soldier', playing:false,training:true, highlight:'http://youtube.com',title:'that save'},
+	{name:'tracer', playing:true,training:false, highlight:'http://youtube.com',title:'Tire action'},
+	{name:'genji', playing:true,training:false, highlight:'http://youtube.com',title:'simple geometry'},
+	{name:'winston', playing:true,training:false, highlight:'http://youtube.com',title:'justice rains'},
+	{name:'lucio', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 	$scope.data.squad["jayjay"].championpool= [
-		{name:'mccree', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+		{name:'mccree', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
-	$scope.data.squad["twix"].championpool= [
-		{name:'mccree', playing:true,training:false, title:'that save'},
-		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+	$scope.data.squad["sauvrinn"].championpool= [
+		{name:'anna', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zarya', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+	];
+	$scope.data.squad["lewcor"].championpool= [
+		{name:'dva', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zarya', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+	];
+	$scope.data.squad["clowntown"].championpool= [
+		{name:'mercy', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'lucio', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 }]);
