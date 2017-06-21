5e57c914aaf2fc1a3e6e56ed8acd457520a1c1c0
Anzumana Taal <anzumana.taal@me.com>
2017-06-21 10:50:56 +0200

    overwatch squad info update

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ myApp.controller('SquadCtrl', ['$scope', function TodoCtrl($scope) {
 	$scope.data.squad["anzumana"].name="Senwyn#2415";
 	$scope.data.squad["jayjay"].name="JayJayBiggie#2793";
 	$scope.data.squad["sauvrinn"].name="Sauvrin#1244";
-	$scope.data.squad["lewcor"].name="lewcor12#";
+	$scope.data.squad["lewcor"].name="DukeofNuts#11761";
 	$scope.data.squad["clowntown"].name="Myles#1857";
+	$scope.data.squad["blaze"].name="Blaze#14341";
+	$scope.data.squad["kaiser"].name="KaiserPodge#1914";
 
 	$scope.data.squad["anzumana"].championpool= [
 	{name:'tracer', playing:true,training:false, highlight:'http://youtube.com',title:'Tire action'},
@@ -38,13 +40,25 @@ myApp.controller('SquadCtrl', ['$scope', function TodoCtrl($scope) {
 	];
 	$scope.data.squad["lewcor"].championpool= [
 		{name:'dva', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
-		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zarya', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'roadhog', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'zarya', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 	$scope.data.squad["clowntown"].championpool= [
 		{name:'mercy', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
 		{name:'lucio', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+	];
+	$scope.data.squad["blaze"].championpool= [
+		{name:'soldier', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'genji', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'hanzo', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'phara', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'mccree', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
+	];
+	$scope.data.squad["kaiser"].championpool= [
+		{name:'reinhard', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'winston', playing:true,training:false, highlight:'http://youtube.com',title:'that save'},
+		{name:'zeniata', playing:true,training:false, highlight:'http://youtube.com',title:'that save'}
 	];
 }]);
